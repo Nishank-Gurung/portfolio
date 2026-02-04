23c0a9fc2d5317b57dcae5d9dd1cee444237e944
Nishank Gurung <gurungnishank@gmail.com>
2026-02-04 11:29:00 +0545

    types addition

---
+++ src/lib/types.ts
@@ -0,0 +1,95 @@
+export type user = {
+    id: number;
+    name: string;
+    email: string;
+    title?: string;
+    image?: string;
+    about?: string;
+    phone?: string;
+    address?: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type project = {
+    id: number;
+    title: string;
+    slug: string;
+    description: string;
+    image: string;
+    url: string;
+    techStack: string[];
+    isFeatured: boolean;
+    link: string;
+    category: string[];
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type experience = {
+    id: number;
+    company: string;
+    position: string;
+    url?: string;
+    skills: string[];
+    startDate: Date;
+    endDate: Date | null;
+    description: string;
+    isCurrent: boolean;
+    type: "FULL_TIME" | "INTERNSHIP" | "FREELANCE"|"CONTRACT"|"PART_TIME";
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type skill = {
+    id: number;
+    name: string;
+    image?: string;
+    category?: string;
+    level?: "BEGINNER" | "INTERMEDIATE" | "ADVANCED" | "EXPERT";
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type socialMedia = {
+    id: number;
+    platform: string;
+    url: string;
+    image: string;
+    order: number;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type education = {
+    id: number;
+    institution: string;
+    degree: string;
+    fieldOfStudy: string;
+    startDate: Date;
+    endDate: Date | null;
+    description: string;
+    isCurrent: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type post = {
+    id: number;
+    title: string;
+    slug: string;
+    excerpt?: string;
+    content: string;
+    coverImage?: string;
+    status: "DRAFT" | "PUBLISHED";
+    publishedAt: Date | null;
+    authorId: number;
+    seoTitle?: string;
+    seoDescription?: string;
+    tag: string[];
+    category: string[];
+    postViews: number;
+    createdAt: Date;
+    updatedAt: Date;
+};
