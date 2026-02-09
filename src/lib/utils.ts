42fe3f1b0a624e514f1f6bf03a70791a56564db6
Nishank Gurung <gurungnishank@gmail.com>
2026-02-09 11:42:00 +0545

    showing toast

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { AxiosError } from "axios";
+import { clsx, type ClassValue } from "clsx";
+import { toast } from "sonner";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const showErrorTost = (error: any) => {
+    if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+    } else if (typeof error === "string") {
+        toast.error(error);
+    } else {
+        toast.error("Something went wrong");
+    }
+};
+
+export const showSuccessToast = (msg: string) => {
+    toast.success(msg);
+};
