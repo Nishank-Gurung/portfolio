b201736b5a927b138cf34e98e13f197dc3ca15a0
Nishank Gurung <gurungnishank@gmail.com>
2026-02-23 12:50:00 +0545

    admin experience page

--- src/app/(admin)/admin/(sidebar)/experiences/page.tsx
+++ src/app/(admin)/admin/(sidebar)/experiences/page.tsx
@@ -1,9 +1,16 @@
 import ExperiencesPage from "@/components/admin/ExperiencesPage";
 
-export default function page() {
+interface Props {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+export default async function page({ searchParams }: Props) {
+const resolvedParams = await searchParams;
+  const error = resolvedParams.error as string;
   return (
     <div>
-        <ExperiencesPage />
+        <ExperiencesPage error={error}/>
     </div>
   )
 }
