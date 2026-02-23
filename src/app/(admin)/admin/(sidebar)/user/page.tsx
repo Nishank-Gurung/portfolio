97c50341e9a3ff92cf300239d8d7d313333113e1
Nishank Gurung <gurungnishank@gmail.com>
2026-02-23 11:10:00 +0545

    admin user page

--- src/app/(admin)/admin/(sidebar)/user/page.tsx
+++ src/app/(admin)/admin/(sidebar)/user/page.tsx
@@ -1,10 +1,16 @@
 import UserPage from "@/components/admin/UserPage";
 
-
-export default function page() {
-  return (
-    <div>
-        <UserPage/>
-    </div>
-  )
+interface Props {
+    searchParams: {
+        [key: string]: string | string[] | undefined;
+    };
+}
+export default async function page({ searchParams }: Props) {
+    const params = await searchParams;
+    const error = params.error as string | undefined;
+    return (
+        <div>
+            <UserPage error={error} />
+        </div>
+    );
 }
