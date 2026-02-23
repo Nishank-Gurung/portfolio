008d2a6dbfd11c02f180eeae877b031b92c3f02e
Nishank Gurung <gurungnishank@gmail.com>
2026-02-23 15:50:00 +0545

    amdin skills page

--- src/app/(admin)/admin/(sidebar)/skills/page.tsx
+++ src/app/(admin)/admin/(sidebar)/skills/page.tsx
@@ -1,10 +1,16 @@
 import SkillsPage from "@/components/admin/SkillsPage";
 
-
-export default function page() {
-  return (
-    <div>
-        <SkillsPage/>
-    </div>
-  )
+interface Props {
+    searchParams: {
+        [key: string]: string | string[] | undefined;
+    };
+}
+export default async function page({ searchParams }: Props) {
+    const resolvedParams = await searchParams;
+    const error = resolvedParams.error as string;
+    return (
+        <div>
+            <SkillsPage error={error} />
+        </div>
+    );
 }
