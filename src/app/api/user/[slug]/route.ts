7a6c87d75f7b8776869396f5a7ff9c27ea113318
Nishank Gurung <gurungnishank@gmail.com>
2026-02-26 11:00:00 +0545

    user slug route

--- src/app/api/user/[slug]/route.ts
+++ src/app/api/user/[slug]/route.ts
@@ -1,7 +1,38 @@
 // import { imagekit } from "@/lib/imagekit";
 // import { educationSchema } from "@/lib/schemas";
-// import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(
+    req: NextRequest,
+    { params }: { params: Promise<{ slug: string }> },
+) {
+    try {
+        const userId = Number((await params).slug);
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+        });
+
+        if (!user) {
+            return NextResponse.json(
+                { success: false, message: "User not found" },
+                { status: 404 },
+            );
+        }
+
+        return NextResponse.json({ success: true, user });
+    } catch (error) {
+        console.error("Fetch error:", error);
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Failed to fetch user",
+                error: error instanceof Error ? error.message : "Unknown error",
+            },
+            { status: 500 },
+        );
+    }
+}
 // export async function POST(
 //     req: NextRequest,
 //     { params }: { params: { slug: string } },
